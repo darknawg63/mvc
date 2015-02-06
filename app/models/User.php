aff22b4aa276873438c57c17f255629d35548f58
darknawg63 <darknawg63@gmail.com>
2015-02-06 21:12:36 +0100

    Added Eloquent ORM

--- app/models/User.php
+++ app/models/User.php
@@ -1,6 +1,13 @@
 <?php
 
-class User
+use Illuminate\Database\Eloquent\Model as Eloquent;
+
+class User extends Eloquent
 {
     public $name;
+
+    // Send an empty array if we don't want timestamps from Eloquent
+    //public $timestamps = [];
+
+    protected $fillable = ['username', 'email'];
 }
